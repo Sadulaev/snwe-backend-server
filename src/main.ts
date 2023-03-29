fdacaab8095dcfdc088b6d49b73c91de5cc6cc97
Ismail <it_temp4@bwg-io.site>
2023-03-30 02:38:48 +0300

    https temp crt

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import fs from 'fs'
 
 async function start() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: fs.readFileSync('../etc/ssl/temp.key'),
+      cert: fs.readFileSync('../etc/ssl/temp.crt'),
+    }
+  });
   app.use(cookieParser());
   app.enableCors({
     credentials: true,
-    origin: 'http://localhost:8081'
+    origin: 'https://80.78.253.83:443'
   });
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
